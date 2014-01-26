7ccd3a3e98a8e74013da0f15ce8f3d701d775e98
jonbrennecke <jpbrennecke@gmail.com>
2014-01-26 12:43:53 -0800

    added ./extras

--- core.js
+++ core.js
@@ -1,28 +1,29 @@
 /** 
  * Copyright 2014 Jon Brennecke
- * Released under the MIT license.
+ * rjseleased under the MIT license.
  *
  */
 
-var R = R || {};
+var rjs = rjs || {};
 
-R.Vector3D = function(x,y,z){
+rjs.Vector3D = function(x,y,z){
 	this.x = x || 0;
 	this.y = y || 0;
 	this.z = z || 0;
 };
 
-R.Vector3D.prototype = {
+rjs.Vector3D.prototype = {
 
 	// set the coordinates of this vector 
-	set : function(v) {
-		this.x = v.x;
-		this.y = v.y;
-		this.z = v.z;
+	set : function(x,y,z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		return this;
 	},
 
 	// find the vector projection in the direction of v 
-	proj : function(v){ return new R.Vector3D(v.x - this.x, v.y - this.y, v.z - this.z) }, // TODO rename!!!
+	proj : function(v){ return new rjs.Vector3D(v.x - this.x, v.y - this.y, v.z - this.z) }, // TODO rename!!!
     
     // find the norm of this vector
     norm : function(){ return Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z) },
@@ -30,22 +31,22 @@ R.Vector3D.prototype = {
 	// return (a copy of) this vector as a unit vector
 	normalize : function(){
 	    var n = this.norm();
-		return new R.Vector3D( this.x / n, this.y / n, this.z / n )
+		return new rjs.Vector3D( this.x / n, this.y / n, this.z / n )
 	},
 
 	// subtract a vector 'v' from the vector 
 	sub : function(v){
-		return new R.Vector3D(this.x - v.x, this.y - v.y, this.z - v.z);
+		return new rjs.Vector3D(this.x - v.x, this.y - v.y, this.z - v.z);
 	},
 
 	// add a vector 'v' to the vector
 	add : function(v){
-		return new R.Vector3D(this.x + v.x, this.y + v.y, this.z + v.z);
+		return new rjs.Vector3D(this.x + v.x, this.y + v.y, this.z + v.z);
 	},
 
 	// multiply components of the vector by a scalar 's'
 	multiply : function(s){
-		return new R.Vector3D(this.x * s, this.y * s, this.z * s);
+		return new rjs.Vector3D(this.x * s, this.y * s, this.z * s);
 	},
 
 	// return the dot product of this vector
@@ -55,7 +56,7 @@ R.Vector3D.prototype = {
 	
 	// return the cross product of this vector
 	cross : function(v){
-	    return new R.Vector3D (
+	    return new rjs.Vector3D (
 		    this.y * v.z - this.z * v.y,
 		    this.z * v.x - this.x * v.z,
 		    this.x * v.y - this.y * v.x);
@@ -63,14 +64,14 @@ R.Vector3D.prototype = {
 
 	// return a new copy of this vector 
 	clone : function(){
-		return new R.Vector3D( this.x, this.y, this.z )
+		return new rjs.Vector3D( this.x, this.y, this.z )
 	}
 };
 
-R.Object3D = function( position ){
+rjs.Object3D = function( position ){
 	this.position = position;
 };
 
-R.Object3D.prototype = {
+rjs.Object3D.prototype = {
 
 };
